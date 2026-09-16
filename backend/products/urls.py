d4be391c9ba3c4b330e9d9bf2f31fb842317294e
Mduduzi Zulu <ministermaestro@gmail.com>
2026-09-16 20:53:42 +0200

    feat: add product catalogue route

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,15 +1,38 @@
 from django.urls import path
-from .views import ProductByBarcodeView, ProductBySkuView
+from .views import (
+    ProductByBarcodeView,
+    ProductBySkuView,
+    ProductListCreateView,
+    )
 
 urlpatterns = [
+    
+#---Product Catalogue
+
+     path(
+         "",
+         ProductListCreateView.as_view(),
+         name="product-list-create",
+     ),
+
+
+#---Cashier URL Endpoints
     path(
         "barcode/<str:barcode>/",
         ProductByBarcodeView.as_view(),
         name="product-by-barcode",
     ),
+
+
     path(
         "sku/<str:sku>/",
         ProductBySkuView.as_view(),
         name="product-by-sku"
     ),
 ]
+
+#------------- Product View or Listing URL EndPoint------------#
+
+
+
+
