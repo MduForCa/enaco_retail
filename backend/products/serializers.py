cefc369ebee06a652ab637c54ecb84d2a03d1fb4
Mduduzi Zulu <ministermaestro@gmail.com>
2026-09-16 17:59:01 +0200

    feat: add product catalogue listing

---
+++ backend/products/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            "id,"
+            "name",
+            "sku",
+            "barcode",
+            "selling_price",
+            "created_at",
+            "updated_at",
+        ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Product
+from .serializers import ProductSerializer
 from .services import find_product_by_barcode, find_product_by_sku
 
 class ProductByBarcodeView(APIView):
@@ -21,7 +22,7 @@ class ProductByBarcodeView(APIView):
             "sku": product.sku,
             "barcode": product.barcode,
             "selling_price": product.selling_price,
-            "stock_quantity": product.stock_quantity,
+           
         }) 
 
 class ProductBySkuView(APIView):
@@ -39,7 +40,25 @@ class ProductBySkuView(APIView):
             "sku": product.sku,
             "barcode": product.barcode,
             "selling_price": product.selling_price,
-            "stock_quantity": product.stock_quantity,
+            
         })
 
+
+class ProductListCreateView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+
+        return Response(serializer.data)
+
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+
+        return Response(serializer.errors, status=400)
+        
             
